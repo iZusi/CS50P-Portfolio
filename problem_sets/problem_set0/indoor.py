44d7fdeb8762526647ccd1a00bc8d3c1647f925b
Iyke <136558859+iZusi@users.noreply.github.com>
2023-09-27 00:30:56 +0100

    Update indoor.py

--- problem_sets/problem_set0/indoor.py
+++ problem_sets/problem_set0/indoor.py
@@ -1,2 +1,8 @@
+# Prompt the user for input and store it in the 'message' variable
 message = input("").lower().strip()
+
+# Convert the input message to lowercase using the .lower() method
+# and remove leading/trailing whitespace using the .strip() method
+
+# Print the modified message
 print(message)
