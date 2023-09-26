e6eb38c7064359c3d61de2782d263cc8ebe99e27
Iyke <136558859+iZusi@users.noreply.github.com>
2023-09-27 00:36:58 +0100

    Update playback.py

--- problem_sets/problem_set0/playback.py
+++ problem_sets/problem_set0/playback.py
@@ -1,3 +1,8 @@
-prompt = input('').strip().replace(' ', '...')
+# Prompt the user for input and store it in the 'prompt' variable
+prompt = input('').strip()
 
+# Replace all spaces in the input with '...' using the .replace() method
+prompt = prompt.replace(' ', '...')
+
+# Print the modified 'prompt'
 print(prompt)
