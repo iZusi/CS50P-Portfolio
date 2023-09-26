2e6ab70c4fa665d0585487723895ff9c2012c67e
Iyke <136558859+iZusi@users.noreply.github.com>
2023-09-27 00:55:53 +0100

    Update einstein.py

--- problem_sets/problem_set0/einstein.py
+++ problem_sets/problem_set0/einstein.py
@@ -1,5 +1,11 @@
+# Prompt the user for the value of 'm' and convert it to an integer
 m = int(input('m: '))
-c = 300000000**2
-e = m*c
 
+# Calculate the square of the speed of light 'c' (approximately 300,000,000 meters per second)
+c = 300000000**2
+
+# Calculate the energy 'e' using the formula E=mc^2, where 'm' is the mass and 'c' is the speed of light squared
+e = m * c
+
+# Print the calculated energy 'e'
 print(e)
