f296bf0f51483d2b0075602ce1a0e420e181a302
Iyke <136558859+iZusi@users.noreply.github.com>
2023-09-26 21:57:16 +0100

    Update camel.py

--- problem_sets/problem_set2/camel.py
+++ problem_sets/problem_set2/camel.py
@@ -1,8 +1,10 @@
-# get user input
+# Get user input and prompt for camelCase text
 prompt = input('camelCase: ').strip()
 
+# Replace specific camelCase words with their snake_case equivalents
 prompt = prompt.replace('name', 'name')
-prompt = prompt.replace('firstName', 'first_name')
-prompt = prompt.replace('preferredFirstName', 'preferred_first_name')
+prompt = prompt.replace('firstName', 'first_name')  # Replace 'firstName' with 'first_name'
+prompt = prompt.replace('preferredFirstName', 'preferred_first_name')  # Replace 'preferredFirstName' with 'preferred_first_name'
 
+# Print the modified string in snake_case format
 print(f'snake_case: {prompt}')
