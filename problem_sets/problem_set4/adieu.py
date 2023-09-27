535ad2ddb6ad3432ec857b099b7e5801e5e592b2
Iyke <136558859+iZusi@users.noreply.github.com>
2023-09-27 21:26:29 +0100

    Update adieu.py

--- problem_sets/problem_set4/adieu.py
+++ problem_sets/problem_set4/adieu.py
@@ -1,29 +1,32 @@
-# Here, we want to implement a program that prompts the user for names, one per line,
-# until the user inputs ctrl-d. We'll assume that the user will input at least one name.
-# Then bid adieu to those names, separating two names with one "and",
-# three names with two commas and one "and", and n names with n - 1 commas and one "and".
+# Program Description:
+# This program prompts the user for names, one per line, until the user inputs Ctrl-D.
+# It assumes that the user will input at least one name and then bids adieu to those names.
+# The program separates two names with one "and," three names with two commas and one "and,"
+# and n names with n - 1 commas and one "and."
 
-
-# create an empty list to store names
+# Create an empty list to store names
 lst = []
 
-# we need to continually prompt the user until they enter ctrl-d to exit the program.
+# Continuously prompt the user until they enter Ctrl-D (EOFError) to exit the program
 while True:
     try:
-        # get user's input
+        # Get the user's input for a name, titlecase it, and remove leading/trailing whitespace
         name = input('Name: ').title().strip()
 
-        # add user's input to list
+        # Add the user's input to the list of names
         lst.append(name)
 
     except EOFError:
         if len(lst) == 1:
+            # If there is only one name, bid adieu to that name
             print(f'\nAdieu, adieu, to {lst[0]}')
             break
         elif len(lst) == 2:
+            # If there are two names, bid adieu to both with "and" between them
             print(f'\nAdieu, adieu, to {lst[0]} and {lst[1]}')
             break
         else:
-            # concatenate and print elements from the list into a single string, separated by a specified delimiter.
+            # If there are more than two names, format and bid adieu to all of them
+            # using commas and "and" as appropriate
             print(f'\nAdieu, adieu, to ' + ', '.join(lst[:-1]) + f', and {lst[-1]}')
             break
