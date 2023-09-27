dba48dac68f2140a06b1dd5cd297db373a5abf35
Iyke <136558859+iZusi@users.noreply.github.com>
2023-09-27 01:34:35 +0100

    Update interpreter.py

--- problem_sets/problem_set1/interpreter.py
+++ problem_sets/problem_set1/interpreter.py
@@ -1,21 +1,26 @@
-# get user input
+# Get user input for a mathematical expression
 expression = (input('Expression: '))
 
-# split user input into three parts with a whitespace and store each in a variable
+# Split the user input into three parts (x, operator, and z) using whitespace as the delimiter
 x, y, z = expression.split(' ')
 
+# Check the operator in the expression
 if '+' in expression:
+    # If '+' is found, perform addition and print the result rounded to one decimal place
     n = float(x) + float(z)
-    print(round(n,1))
+    print(round(n, 1))
 elif '-' in expression:
+    # If '-' is found, perform subtraction and print the result rounded to one decimal place
     m = float(x) - float(z)
-    print(round(m,1))
+    print(round(m, 1))
 elif '/' in expression:
+    # If '/' is found, check if dividing by zero and handle the error, or perform division and print the result rounded to one decimal place
     if float(z) == 0:
         print('Math Error!')
     else:
         o = float(x) / float(z)
-        print(round(o,1))
+        print(round(o, 1))
 elif '*' in expression:
+    # If '*' is found, perform multiplication and print the result rounded to one decimal place
     p = float(x) * float(z)
-    print(round(p,1))
+    print(round(p, 1))
