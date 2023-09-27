3cff6bc196c8444e6df26f9e49103e0ac1438c01
Iyke <136558859+iZusi@users.noreply.github.com>
2023-09-27 21:31:16 +0100

    Update game.py

--- problem_sets/problem_set4/game.py
+++ problem_sets/problem_set4/game.py
@@ -1,37 +1,48 @@
 # Creating a guessing game
 
+# Import the 'random' module for generating random numbers
 import random
 
-
+# Start an infinite loop to allow the game to restart
 while True:
     try:
-        # prompt user for input
+        # Prompt the user for the game level and convert it to an integer
         n = int(input("Level: "))
 
+        # Check if the user entered a negative number and continue the loop if so
         if n < 0:
             continue
         else:
-            # generate a random number between 1 and n
+            # Generate a random number between 1 and n (inclusive)
             output = random.randint(1, n)
     except ValueError:
+        # Handle non-integer inputs gracefully and continue the loop
         continue
 
+    # Start an inner loop for the guessing game
     while True:
         try:
-            # prompt user again for another input
+            # Prompt the user to guess the number and convert their input to an integer
             guess = int(input("Guess: "))
 
+            # Check if the user entered a negative number and continue the loop if so
             if guess < 0:
                 continue
+            # Check if the guess is smaller than the random number
             elif guess < output:
                 print("Too small!")
                 continue
+            # Check if the guess is larger than the random number
             elif guess > output:
                 print("Too large!")
                 continue
+            # Check if the guess is equal to the random number, indicating a correct guess
             elif guess == output:
                 print("Just right!")
-                break
+                break  # Exit the inner loop when the guess is correct
         except ValueError:
+            # Handle non-integer inputs gracefully and continue the loop
             pass
+
+    # Exit the outer loop to end the game
     break
