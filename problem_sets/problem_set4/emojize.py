d8ae5479e9d2334c69804644555382ac8c179955
Iyke <136558859+iZusi@users.noreply.github.com>
2023-09-27 21:17:34 +0100

    Update emojize.py

--- problem_sets/problem_set4/emojize.py
+++ problem_sets/problem_set4/emojize.py
@@ -1,10 +1,11 @@
 from emoji import emojize
 
-# prompt user for input
+# Prompt the user for input and store it in a variable
 text = input('Input: ')
 
-# emojize user's input
-text = emojize(text, language = 'alias')
+# Emojize the user's input using the 'emojize' function
+# The 'language' parameter is set to 'alias' to interpret emoji aliases in the input
+text = emojize(text, language='alias')
 
-# print output
+# Print the emojized output, which replaces emoji aliases with actual emojis
 print(f'Output: {text}')
